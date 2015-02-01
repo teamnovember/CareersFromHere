ed75128d9d4efc5808ab4422e4b79d42ccc74723
Theodor <tdn26@cam.ac.uk>
2015-02-01 12:05:13 +0000

    added ability to record; used whammy.js solution; it is slow

--- public/javascripts/recorder.js
+++ public/javascripts/recorder.js
@@ -17,4 +17,32 @@ function initialiseRecorder() {
     });
 };
 
+var rafId;
+var frames = [];
+
+function startRecording() {
+    var canvas = document.createElement("canvas");
+    var ctx = canvas.getContext("2d");
+
+    var width = canvas.width;
+    var height = canvas.height;
+
+    function drawVideoFrame(time) {
+        rafId = requestAnimationFrame(drawVideoFrame);
+        ctx.drawImage(recorder, 0, 0, width, height);
+        frames.push(canvas.toDataURL("image/webp", 1));
+    };
+
+    rafId = requestAnimationFrame(drawVideoFrame);
+};
+
+function stopRecording() {
+    cancelAnimationFrame(rafId);
+
+    var webmBlob = Whammy.fromImageArray(frames, 1000 / 60);
+
+    var player = document.getElementById("video-player");
+    player.src = window.URL.createObjectURL(webmBlob);
+};
+
 document.addEventListener("DOMContentLoaded", function() { initialiseRecorder(); }, false);
