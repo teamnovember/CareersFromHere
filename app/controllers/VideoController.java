6298460120cca153cf876a200e2924669f249a33
Edgar <el398@cam.ac.uk>
2015-02-01 21:14:47 +0000

    video gallery alpha 2

--- app/controllers/VideoController.java
+++ app/controllers/VideoController.java
@@ -11,12 +11,13 @@ import java.util.List;
 public class VideoController extends Controller {
     public static Result index() {
 
-        // Temporary data, use
+        // Temporary data, TEDx videos sorted by popularity
         List<Video> videoList = new ArrayList<Video>();
-        videoList.add(new Video("My Career in ML", "Blah", "http://placehold.it/275x177/E8117F/FFFFFF", Duration.ofSeconds(163)));
-        videoList.add(new Video("My Career in Prolog", "Blah", "http://placehold.it/275x177/E8117F/FFFFFF", Duration.ofSeconds(155)));
-        videoList.add(new Video("My Failed Career in Java", "Blah", "http://placehold.it/275x177/E8117F/FFFFFF", Duration.ofSeconds(258)));
-        videoList.add(new Video("How I lecture both CC and DBs", "Blah", "http://placehold.it/275x177/E8117F/FFFFFF", Duration.ofSeconds(863)));
+        videoList.add(new Video("Beatbox brilliance", "Blah", "http://img.youtube.com/vi/GNZBSZD16cY/0.jpg", Duration.ofSeconds(163)));
+        videoList.add(new Video("Hackschooling makes me happy", "Blah", "http://img.youtube.com/vi/h11u3vtcpaY/0.jpg", Duration.ofSeconds(155)));
+        videoList.add(new Video("My philosophy for a happy life", "Blah", "http://img.youtube.com/vi/36m1o-tM05g/0.jpg", Duration.ofSeconds(258)));
+        videoList.add(new Video("Why I stopped watching porn", "Blah", "http://img.youtube.com/vi/gRJ_QfP2mhU/0.jpg", Duration.ofSeconds(863)));
+        videoList.add(new Video("Coming out of your closet", "Blah", "http://img.youtube.com/vi/kSR4xuU07sc/0.jpg", Duration.ofSeconds(863)));
 
         return ok(index.render(videoList));
     }
