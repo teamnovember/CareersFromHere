3eefc7f377989334295a0a97cae7ebfdfd214a9b
Saravanan <saravanans23@gmail.com>
2015-02-04 14:47:30 +0000

    Added database evolution file

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,30 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table video (
+  id                        bigint not null,
+  title                     varchar(255),
+  description               varchar(255),
+  thumbnail_path            varchar(255),
+  duration                  integer,
+  approved                  boolean,
+  constraint pk_video primary key (id))
+;
+
+create sequence video_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists video;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists video_seq;
+
