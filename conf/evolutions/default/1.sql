a4d405bcf279585db76dd2da9e423bd61be289b0
Louise <led45@cam.ac.uk>
2015-02-05 17:49:53 +0000

    Changed database to the SRCF database

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,54 +4,69 @@
 # --- !Ups
 
 create table question (
-  question_id               bigint not null,
+  question_id               bigint auto_increment not null,
   text                      varchar(255),
+  duration                  integer,
   SCHOOL                    bigint,
   constraint pk_question primary key (question_id))
 ;
 
 create table school (
-  school_id                 bigint not null,
+  school_id                 bigint auto_increment not null,
   name                      varchar(255),
   constraint pk_school primary key (school_id))
 ;
 
+create table student (
+  id                        bigint auto_increment not null,
+  name                      varchar(255),
+  password                  varchar(255),
+  email                     varchar(255),
+  school_school_id          bigint,
+  constraint pk_student primary key (id))
+;
+
+create table user (
+  id                        bigint auto_increment not null,
+  name                      varchar(255),
+  password                  varchar(255),
+  email                     varchar(255),
+  school_school_id          bigint,
+  constraint pk_user primary key (id))
+;
+
 create table video (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   title                     varchar(255),
   description               varchar(255),
   thumbnail_path            varchar(255),
   duration                  integer,
-  approved                  boolean,
+  approved                  tinyint(1) default 0,
   constraint pk_video primary key (id))
 ;
 
-create sequence question_seq;
-
-create sequence school_seq;
-
-create sequence video_seq;
-
 alter table question add constraint fk_question_school_1 foreign key (SCHOOL) references school (school_id) on delete restrict on update restrict;
 create index ix_question_school_1 on question (SCHOOL);
+alter table student add constraint fk_student_school_2 foreign key (school_school_id) references school (school_id) on delete restrict on update restrict;
+create index ix_student_school_2 on student (school_school_id);
+alter table user add constraint fk_user_school_3 foreign key (school_school_id) references school (school_id) on delete restrict on update restrict;
+create index ix_user_school_3 on user (school_school_id);
 
 
 
 # --- !Downs
 
-SET REFERENTIAL_INTEGRITY FALSE;
+SET FOREIGN_KEY_CHECKS=0;
 
-drop table if exists question;
+drop table question;
 
-drop table if exists school;
+drop table school;
 
-drop table if exists video;
+drop table student;
 
-SET REFERENTIAL_INTEGRITY TRUE;
+drop table user;
 
-drop sequence if exists question_seq;
+drop table video;
 
-drop sequence if exists school_seq;
-
-drop sequence if exists video_seq;
+SET FOREIGN_KEY_CHECKS=1;
 
