05de2be8319094045e67b0a9a743e3b8f3f76fec
Edgar <el398@cam.ac.uk>
2015-02-05 13:45:39 +0000

    added admin dashboard

---
+++ app/controllers/AdminController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import models.User;
+import models.Video;
+import play.mvc.*;
+import views.html.admin.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminController extends Controller {
+    public static Result index() {
+        User u = new User(1,"Edgaras Liberis","blahblah","el398@cam.ac.uk");
+        return ok(dashboard.render(u));
+    }
+}
