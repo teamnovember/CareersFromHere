dc8d5ef6bea7974e99ace6f3c4d0c740cbca66b8
Louise <led45@cam.ac.uk>
2015-02-21 19:48:48 +0000

    Changed SuperAdmin so it inherits from User instead of Admin to fix db stuff

--- app/models/SuperAdmin.java
+++ app/models/SuperAdmin.java
@@ -1,5 +1,7 @@
 package models;
 
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.ExpressionList;
 import views.forms.UserForm;
 
 import javax.persistence.DiscriminatorValue;
@@ -11,7 +13,7 @@ import java.util.List;
  */
 @Entity
 @DiscriminatorValue("superadmin")
-public class SuperAdmin extends Admin {
+public class SuperAdmin extends User {
 
     public SuperAdmin(String name, String password, String email, School school){
         super(name, password, email, school);
@@ -30,5 +32,41 @@ public class SuperAdmin extends Admin {
     public List<School> getSchools() {
         SchoolDAO dao = new SchoolDAO();
         return dao.getAllSchool();
+    }
+
+    public List<Video> getUnapprovedVideos(){
+        ExpressionList<Video> expList = Video.find.where().eq("approved",0);
+        List<Video> videos = expList.findList();
+
+        return videos;
+    }
+
+    public List<Video> getApprovedVideos(){
+        ExpressionList<Video> expList = Video.find.where().eq("approved",1);
+        List<Video> videos = expList.findList();
+
+        return videos;
+    }
+
+    public List<Video> getAllVideos(){
+        List<Video> videos = Ebean.find(Video.class).select("*").orderBy("Video.approved, approved asc").findList();
+        return videos;
+    }
+
+    public void addQuestion(String question, int duration){
+        School school = this.getSchool();
+        school.addQuestion(question,duration);
+    }
+
+    public List<Student> getStudents() {
+        School school = this.getSchool();
+        List<Student> students = Student.find.where().eq("school",school).findList();
+        return students;
+    }
+
+    public List<Alumni> getAlumni() {
+        School school = this.getSchool();
+        List<Alumni> alumni = Alumni.find.where().eq("school",school).findList();
+        return alumni;
     }
 }
